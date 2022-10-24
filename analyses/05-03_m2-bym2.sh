722663c7af8bd983270e8d30715eb0b5610be978
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-10-24 15:41:53 +0200

    updated time req

--- analyses/05-03_m2-bym2.sh
+++ analyses/05-03_m2-bym2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="i-bym2"
 #SBATCH --cpus-per-task=128
 #SBATCH --mem-per-cpu=512
-#SBATCH --time=12:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
 #SBATCH --mail-type=begin,end,fail
 
