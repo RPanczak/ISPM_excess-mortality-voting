b15d24e4c58cd795e7510dee3c3128730c726a56
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-10-24 15:47:17 +0200

    renames for better mgmt

---
+++ analyses/06-01_m2-bym2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="i-bym2"
+#SBATCH --cpus-per-task=128
+#SBATCH --mem-per-cpu=512
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
+#SBATCH --mail-type=begin,end,fail
+
+ml vital-it
+ml R/3.6.1
+
+Rscript ~/ISPM_excess-mortality-voting/analyses/06-02_m2-bym2.R
--- analyses/06-02_m2-bym2.R
+++ analyses/06-02_m2-bym2.R
@@ -15,7 +15,9 @@ data <- read_rds("data/BfS-closed/monthly_deaths/w_deaths_2015_2020_year_fin.Rds
   select(-(ARGRNR:ARNAME)) %>%
   filter(age != "<40") %>% 
   # strata with double zeroes seem to be crashing !!!
-  filter(pop_mid_poi > 0) 
+  filter(pop_mid_poi > 0) %>% 
+  # only data till 2019
+  filter(year < 2020)
 
 ### INLA setup
 
