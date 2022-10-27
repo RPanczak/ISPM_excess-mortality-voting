48b6e542d92a14d76a2e233483487d66e1eed4a9
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-10-27 15:25:25 +0200

    style & formatting

--- R/downscale_year.R
+++ R/downscale_year.R
@@ -1,46 +1,57 @@
-# canton_data = exp_deaths_2020_kt %>% 
+# sample data 
+# canton_data = exp_deaths_2020_kt %>%
 #   filter(it <= 100)
 # munici_data = w_deaths_2020_year_fin
 
-
-downscale_year = function(canton_data,munici_data) {
+downscale_year = function(canton_data, munici_data) {
+  
   # left_join
-  mm = munici_data %>% 
-    ungroup() %>% 
-    tidyr::expand_grid(it=unique(canton_data$it)) %>% 
-    dplyr::left_join(canton_data,by = c("year", "canton", "age_group", "sex", "it"))
+  mm = munici_data %>%
+    tidyr::expand_grid(it = unique(canton_data$it)) %>%
+    dplyr::left_join(canton_data, by = c("canton", "age_group", "sex", "it"))
+  
   # compute downscale factor p
-  pp = mm %>% 
-    dplyr::group_by(canton,age_group,sex,it) %>% 
-    dplyr::mutate(p=observed/sum(observed))
-  # function to distribute deaths deaths from gemeinde to grid using weights
-  distribute_deaths <- function(n, d, w) {
-    l <- length(d)
-    r <- rmultinom(n, size = d, prob = w + 1e-10) # add small number to avoid numerical issues with pop=0
+  # ie. proportion of canton deaths within strata and community (mind the iteration)
+  pp = mm %>%
+    dplyr::group_by(canton, age_group, sex, it) %>%
+    dplyr::mutate(p = observed / sum(observed))
+  
+  # function to distribute deaths deaths from canton to community using weights 
+  distribute_deaths = function(n, d, w) {
+    l = length(d)
+    r = rmultinom(n, size = d, prob = w + 1e-10) # add small number to avoid numerical issues with pop=0
     return(r)
   }
+  
   # distribute
-  dd = pp %>% 
-    dplyr::group_by(canton,age_group,sex,it) %>% 
-    dplyr::filter(exp_deaths>0,p>0) %>% 
-    dplyr::mutate(munici_exp_deaths = distribute_deaths(n=1,exp_deaths,p))
+  dd = pp %>%
+    dplyr::group_by(canton, age_group, sex, it) %>%
+    dplyr::filter(exp_deaths > 0, p > 0) %>%
+    dplyr::mutate(munici_exp_deaths = distribute_deaths(n = 1, exp_deaths, p))
+  
   # compute excess
-  ee = mm %>% 
-    dplyr::left_join(dd,by = c("year", "canton", "GMDNR",
-                               "age_group", "sex", "observed", "pop_mid_poi",
-                               "it", "exp_deaths", "observed2")) %>% 
-    tidyr::replace_na(list(munici_exp_deaths=0)) %>% 
-    dplyr::mutate(munici_excess = observed-munici_exp_deaths)
+  ee = mm %>%
+    dplyr::left_join(dd, by = c(
+      "canton", "GMDNR",
+      "age_group", "sex", "observed", "pop_mid_poi",
+      "it", "exp_deaths", "observed2"
+    )) %>%
+    tidyr::replace_na(list(munici_exp_deaths = 0)) %>%
+    dplyr::mutate(munici_excess = observed - munici_exp_deaths)
   
   return(ee)
 }
 
-if(FALSE) {
-  dd %>% 
-    filter(canton=="BE",
-           age_group=="80+",
-           sex=="Female",
-           it==3) %>% 
-    summarise(exp_deaths=max(exp_deaths),
-              munici_exp_deaths=sum(munici_exp_deaths))
+if (FALSE) {
+  dd %>%
+    filter(
+      canton == "BE",
+      age_group == "80+",
+      sex == "Female",
+      it == 3
+    ) %>%
+    summarise(
+      exp_deaths = max(exp_deaths),
+      munici_exp_deaths = sum(munici_exp_deaths)
+    )
 }
